9bb029410a5974335e0b8e0d4be95d680ff8106b
Piyush Shukla <74832139+panditofcodes@users.noreply.github.com>
2024-12-29 22:21:50 +0530

    Initial commit with some development.

---
+++ finance_manager/account/doctype/liabilities/test_liabilities.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, PANDITOFCODES and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLiabilities(FrappeTestCase):
+	pass
---
+++ finance_manager/categories/doctype/category/category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, PANDITOFCODES and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Category", {
+// 	refresh(frm) {
+
+// 	},
+// });
